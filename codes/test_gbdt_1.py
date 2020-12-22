96e01806eb9628d04c21529679ae7f2d0e03ac76
calibertytz <calibertytz@163.com>
2020-12-22 18:31:38 +0800

    update

--- codes/test_gbdt_1.py
+++ codes/test_gbdt_1.py
@@ -25,13 +25,13 @@ df_train = pd.read_csv(train_path)
 
 num_leaves = 32
 num_round = 2000
-learning_rate = 1e-2
+learning_rate = 3e-2
 feature_fraction = 0.8
 bagging_fraction = 0.8
 bagging_freq = 10
 extra_trees = True
 
-for num_leaves in tqdm([16, 32, 64, 96, 128, 164, 192]):
+for feature_fraction in tqdm([0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99]):
     common_params = {'num_thread': 64,
                      'num_leaves': num_leaves,
                      'metric': 'binary_error',
