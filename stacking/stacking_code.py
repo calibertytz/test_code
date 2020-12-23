fbc338d60107812f6f8d8b933df3110131d80e4e
calibertytz <calibertytz@163.com>
2020-12-23 20:34:23 +0800

    update

--- stacking/stacking_code.py
+++ stacking/stacking_code.py
@@ -14,7 +14,7 @@ df_train = pd.read_csv(train_path)
 x_train, y_train = df_train.drop(columns=['label']).values, df_train['label'].values
 x_test, y_test = df_test.drop(columns=['label']).values, df_test['label'].values
 
-'''
+
 num_round = 2000
 
 #0.20397999999999997
@@ -34,11 +34,13 @@ dart_param = {'num_thread': 64, 'num_leaves': 128, 'metric': 'binary_error', 'ob
 
 kfold = KFold(n_splits=5)
 out_list = []
-
+test_out_list = []
 for train_index, test_index in kfold.split(x_train):
 
     X_train_, X_test_ = x_train[train_index], x_train[test_index]
     y_train_, y_test_ = y_train[train_index], y_train[test_index]
+    X_test__, y_test__ = x_test[test_index], y_test[test_index]
+
     train_data = lgb.Dataset(data=X_train_, label=y_train_)
 
     bst_gbdt = lgb.train(gbdt_param, train_data, num_round)
@@ -49,20 +51,32 @@ for train_index, test_index in kfold.split(x_train):
     goss_out = bst_goss.predict(X_test_)
     dart_out = bst_goss.predict(X_test_)
 
+    gbdt_out_test = bst_gbdt.predict(X_test__)
+    goss_out_test = bst_goss.predict(X_test__)
+    dart_out_test = bst_goss.predict(X_test__)
+
     out = pd.DataFrame()
     out['gbdt'] = gbdt_out
     out['goss'] = goss_out
     out['dart'] = dart_out
     out['label'] = y_test_
 
+    out_test = pd.DataFrame()
+    out_test['gbdt'] = gbdt_out_test
+    out_test['goss'] = goss_out_test
+    out_test['dart'] = dart_out_test
+    out_test['label'] = y_test__
+
     out_list.append(out)
+    test_out_list.append(out_test)
 
 out_ = pd.concat(out_list)
+out_test = pd.concat(test_out_list)
 
 out_.to_csv('out_.csv', index=False)
-'''
+out_test.to_csv('out_test.csv', index=False)
 
-out_ = pd.read_csv('out_.csv')
+print('model done!')
 
 # our data
 '''
@@ -77,35 +91,66 @@ df_train_filled = df_train.fillna(df_train.median())
 x_train_filled = df_train_filled.drop(columns=['label'])
 x_train_filled = x_train_filled.dropna(axis=1)
 
+df_test_filled = df_test.fillna(df_test.median())
+x_test_filled = df_test_filled.drop(columns=['label'])
+x_test_filled = x_test_filled.dropna(axis=1)
+
 # tsne
 
-tsne = TSNE(n_components=3, n_jobs=-1)
-tsne.fit_transform(x_train_filled)
-df_tsne_embedding = pd.DataFrame(tsne.embedding_)
+tsne_train = TSNE(n_components=3, n_jobs=-1)
+tsne_train.fit(x_train_filled)
+df_tsne_train = pd.DataFrame(tsne_train.embedding_)
 
-df_tsne_embedding.to_csv('tsne_result.csv', index=False)
+tsne_test = TSNE(n_components=3, n_jobs=-1)
+tsne_test.fit(x_test_filled)
+df_tsne_test = pd.DataFrame(tsne_test.embedding_)
+
+df_tsne_train.to_csv('tsne_result_train.csv', index=False)
+df_tsne_test.to_csv('tsne_result_test.csv', index=False)
+
 print('tsne done!')
 
 # knn
+# train
 neigh = NearestNeighbors(n_neighbors=5)
-neigh.fit(df_tsne_embedding.values)
-knn_index = neigh.kneighbors(df_tsne_embedding.values, 5, return_distance=False)
-knn_result = []
+neigh.fit(df_tsne_train.values)
+knn_index_train = neigh.kneighbors(df_tsne_train.values, 5, return_distance=False)
+knn_result_train = []
 
-for index in knn_index:
-    temp = df_tsne_embedding.iloc[index, :].mean().values
-    knn_result.append(temp)
-df_knn_result = pd.DataFrame(np.array(knn_result))
+for index in knn_index_train:
+    temp = df_tsne_train.iloc[index, :].mean().values
+    knn_result_train.append(temp)
+df_knn_result_train = pd.DataFrame(np.array(knn_result_train))
 
-out = pd.concat([out_, df_tsne_embedding, df_knn_result], axis=1, ignore_index=True)
+out_train = pd.concat([out_, df_tsne_train, df_knn_result_train], axis=1, ignore_index=True)
+
+# test
+neigh = NearestNeighbors(n_neighbors=5)
+neigh.fit(df_tsne_test.values)
+knn_index_test = neigh.kneighbors(df_tsne_test.values, 5, return_distance=False)
+knn_result_test = []
+
+for index in knn_index_test:
+    temp = df_tsne_test.iloc[index, :].mean().values
+    knn_result_test.append(temp)
+df_knn_result_test = pd.DataFrame(np.array(knn_result_test))
+
+out_test = pd.concat([out_test, df_tsne_test, df_knn_result_test], axis=1, ignore_index=True)
 
 # mean min max std
-out['std'] = out.std(axis=1)
-out['mean'] = out.mean(axis=1)
-out['min'] = out.min(axis=1)
-out['max'] = out.max(axis=1)
+out_train['std'] = out_train.std(axis=1)
+out_train['mean'] = out_train.mean(axis=1)
+out_train['min'] = out_train.min(axis=1)
+out_train['max'] = out_train.max(axis=1)
 
-out.to_csv('out.csv', index=False)
+out_train.to_csv('out_train.csv', index=False)
+
+out_test['std'] = out_test.std(axis=1)
+out_test['mean'] = out_test.mean(axis=1)
+out_test['min'] = out_test.min(axis=1)
+out_test['max'] = out_test.max(axis=1)
+
+out_test.to_csv('out_test.csv', index=False)
 
 
 
